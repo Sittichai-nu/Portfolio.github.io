3c2a057328b4ef3af89816238a5c8d6bf3fad158
Sittichai Chaikamol <nu@mcp.local>
2020-02-25 15:17:16 -0600

    update

---
+++ asset/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#project").on("click", function(){
+        $("#project").html('<div>Event Spite</div>' + '<a id="link" href="https://leola90.github.io/eventspite/">Go to Event Spite</a>' + '<div><a id="link" href="https://github.com/leola90/eventspite">Github</a></div>');
+       
+    });
+
+});
